580ffbb46f560c43e150b37c43552eb0b7a537df
Tania Samoilenko <80690310+ania0005@users.noreply.github.com>
2024-03-13 16:24:24 +0100

    Create constants.ts

---
+++ LGP_front/src/utils/constants.ts
@@ -0,0 +1,22 @@
+import exp from 'constants';
+import friend1 from '../images/friend1.jpg';
+import friend2 from '../images/friend2.jpg';
+import friend3 from '../images/friend3.jpg';
+import friend4 from '../images/friend4.jpg';
+import friend5 from '../images/friend5.jpg';
+import friend6 from '../images/friend6.jpg';
+import friend7 from '../images/friend7.jpg';
+import friend8 from '../images/friend8.jpg';
+import friend9 from '../images/friend9.jpg';
+
+export const navItems: {item: string, path: string}[] = [
+    { item: "Home", path: "/" },
+    { item: "About me", path: "/about" },
+    { item: "Contacts", path: "/contacts" },
+    { item: "Star Wars", path: "/starwars" },
+  ];
+export const friends = [
+    friend1, friend2, friend3, friend4, friend5, friend6, friend7, friend8, friend9
+];
+
+export const month = 1000*60*60*24*30;
